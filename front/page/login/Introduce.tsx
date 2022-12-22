a3f46d0fc671838663123234cd4956fe55b7d24d
윤선용 <sunyong9059@naver.com>
2022-12-22 23:15:57 +0900

    Introduce 페이지 동영상 제외 버전

--- front/page/login/Introduce.tsx
+++ front/page/login/Introduce.tsx
@@ -1,57 +1,52 @@
 import React from 'react'
 import { Text, View, StyleSheet, Button } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
+import Video from 'react-native-video'
 
-const Introduce = ({navigation}: any) => {
+const Introduce = ({navigation}:any) => {
   return (
-    <>
-    <Test/>
-    <View style={styles.sort}>
-      <Logo>
-        <Text>로고들어감</Text>
-      </Logo>
-    <View style={styles.button}>
-      <Button
-        title='로그인'
-        onPress={()=> navigation.navigate("Login")}
+    <SafeAreaView style={styles.sort}>
+     
+      <View style={styles.button}>
+        <Button
+          title='로그인'
+          onPress={() => navigation.navigate("Login")}
         />
-       <Button
-        title='회원 가입'
-        onPress={()=> navigation.navigate("Signup")}
-        />  
-    </View>
-        </View>
-    </>
+      </View>
+      <View  style={styles.button}>
+         <Button
+          color={'white'}
+          title='회원가입'
+          onPress={() => navigation.navigate("Signup")}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
-const Test = styled.View`
-  width: 100%;
-  height: 35px;
-  background-color: red;
-`
-
-const Logo = styled.View`
-  width: 70%;
-  height: 500px;
-  border: 1px solid red;
-`
-
 const styles = StyleSheet.create({
   sort: {
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center",
-    backgroundColor:"#F5EBE0"
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#FFE3ED'
   },
   button: {
     backgroundColor: '#BEEBE9',
-    borderColor: 'blue',
-    borderWidth: 1,
+    width: 280,
     borderRadius: 15,
-    width: 270,
-    marginTop: 40     
- }
+    marginBottom: 10,
+    borderColor: 'white',
+    borderWidth: 1,
+  }
 });
+
+const Logo = styled.View`
+  width: 280px;
+  height: 500px;
+  border: 1px solid red;
+  margin: 40px 0 60px 0;
+`
+
 
 export default Introduce
