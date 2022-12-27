d20705d997e046ff8cb23db30a679117d0c8235c
윤선용 <sunyong9059@naver.com>
2022-12-27 22:27:19 +0900

    회원가입 페이지 기초안

--- front/page/login/Signup.tsx
+++ front/page/login/Signup.tsx
@@ -1,23 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react'
+import { StyleSheet, Text, View, Button} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { TextInput } from 'react-native-gesture-handler'
+import styled from 'styled-components/native'
 
-const Signup = ({navigation} : any) => {
+const Signup = ({ navigation }: any) => {
 
   return (
-    <View style={styles.container}>
-        <Text>2번째 홈입니다</Text>
-    </View>
+    <SafeAreaView style={styles.sort}>
+      <Logo />
+      <View style={{flexDirection:'row'}}>
+        <TextInput
+          style={styles.customInput}
+          placeholder='닉네임을 입력해주세요'
+        />
+       <Text style={styles.asd}>asd</Text>
+      </View>
+      <TextInput
+          style={styles.customInput}
+          placeholder='비밀번호를 입력해주세요'
+        />
+        <TextInput
+          style={styles.customInput}
+          placeholder='비밀번호를 한번 더 입력하세요'
+        />
+        <View style={{flexDirection:'row'}}>
+        <TextInput
+          style={styles.customInput}
+          placeholder='닉네임을 입력해주세요'
+        />
+       <Text style={styles.asd}>asd</Text>
+      </View>
+      <View style={{flexDirection:'row'}}>
+        <TextInput
+          style={styles.customInput}
+          placeholder='닉네임을 입력해주세요'
+        />
+       <Text style={styles.asd}>asd</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  sort: {
     flex: 1,
-    backgroundColor: 'skyblue',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFE3ED'
   },
+  customInput: {
+    padding: 10,
+    fontSize: 18,
+    backgroundColor: "#eee",
+    width: 230,
+    borderRadius: 8,
+    borderWidth: 1,
+    marginTop:30
+  },
+  button: {
+    flexDirection: "row",
+    backgroundColor: '#9BE3DE',
+    width: 10,
+    borderRadius: 4,
+  },
+  asd: {
+    backgroundColor:'red'
+
+  }
 });
+
+const Logo = styled.View`
+  width: 70%;
+  height: 100px;
+  margin-top: 10%;
+  margin-bottom: 30px;
+  border: 1px solid red;
+`
 
 export default Signup
