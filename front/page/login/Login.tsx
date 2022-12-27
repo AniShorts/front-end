25b354482bebefa6d10e2667acf5c2dd824108df
윤선용 <sunyong9059@naver.com>
2022-12-27 21:17:15 +0900

    로그인 페이지 초안

--- front/page/login/Login.tsx
+++ front/page/login/Login.tsx
@@ -1,74 +1,70 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react'
+import { Text, View, StyleSheet, Button } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
 
 const Login = ({ navigation }: any) => {
-
   return (
-    <>
-      <Test />
-      <View style={styles.sort} >
-        <Logo>
-          <Text>로고임</Text>
-        </Logo>
-        <TextInput
-          style={styles.customInput}
-          placeholder='닉네임을 입력해주세요'
+    <SafeAreaView style={styles.sort}>
+      <Logo />
+      <Text>ID</Text>
+      <TextInput
+        style={styles.customInput}
+        placeholder='닉네임을 입력해주세요'
+      />
+      <Text>Password</Text>
+      <TextInput
+        style={styles.customInput}
+        placeholder='비밀번호를 입력해주세요'
+      />
+      <View style={styles.button}>
+        <Button
+          title='로그인'
+          color={'#FFFDF9'}
         />
-        <TextInput
-          style={styles.customInput}
-          placeholder='비밀번호를 입력해주세요'
-        />
-        <View style={styles.button}>
-          <Button
-            title='로그인'
-            onPress={() => navigation.navigate("Login")}
-          />
-          <Button
-            title='회원 가입'
-            onPress={() => navigation.navigate("Signup")}
-          />
-        </View>
       </View>
-    </>
-  );
+      <View style={styles.alignText}>
+        <Text onPress={() => navigation.navigate("FindId")}>아이디 찾기 </Text>
+        <Text> | </Text>
+        <Text onPress={() => navigation.navigate("FindPassword")}> 비밀번호 찾기</Text>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   sort: {
+    flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: '#FFE3ED'
   },
   button: {
-    backgroundColor: '#BEEBE9',
-    borderColor: 'blue',
-    borderWidth: 1,
-    borderRadius: 15,
-    width: 270,
-    marginTop: 40,
-    marginBottom: 20
+    backgroundColor: '#9BE3DE',
+    width: 310,
+    borderRadius: 4,
+    marginTop: 20
   },
   customInput: {
     padding: 10,
-    marginTop: 30,
     fontSize: 18,
-    // textAlign: 'center',
-    backgroundColor: 'red',
-    width: 320,
-    borderRadius:8
+    backgroundColor: "#eee",
+    width: 300,
+    borderRadius: 8,
+    marginBottom: 12,
+    borderWidth: 1,
   },
+  alignText: {
+    flexDirection: "row",
+    marginTop: 20
+  }
 });
-
-const Test = styled.View`
-  width: 100%;
-  height: 35px;
-  background-color: red;
-`
 
 const Logo = styled.View`
   width: 70%;
   height: 100px;
   margin-top: 10%;
+  margin-bottom: 10%;
   border: 1px solid red;
 `
 
