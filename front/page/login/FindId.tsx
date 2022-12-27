240c3feaf1778d285eec1cc394131562db1b2b44
윤선용 <sunyong9059@naver.com>
2022-12-27 21:15:35 +0900

    FindId, FindPassword page 생성

---
+++ front/page/login/FindId.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const FindId = ({navigation} : any) => {
+
+  return (
+    <View>
+        <Text>아이디 찾는 컴포넌트</Text>
+    </View>
+  );
+}
+
+export default FindId
