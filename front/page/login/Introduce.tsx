c130ed92fdc979c7c32e86afe3eac15e263a38e8
윤선용 <sunyong9059@naver.com>
2023-01-03 13:58:54 +0900

    버튼 TouchableOpacity로 변경 완료(아이콘 추가, 세세한 css수정 필요)

--- front/page/login/Introduce.tsx
+++ front/page/login/Introduce.tsx
@@ -1,69 +1,35 @@
 import React from 'react'
-import { View, StyleSheet, Button, Text, TouchableOpacity} from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import {TouchableOpacity } from 'react-native'
 import styled from 'styled-components/native'
 import Video from 'react-native-video'
 
 const Introduce = ({ navigation }: any) => {
   return (
-    <SafeAreaView style={styles.sort}>
+    <Sort>
       <Logo />
-      <View style={styles.kakaoButton}>
-        <Button
-          title='카카오로 빠르게 시작하기'
-        />
-      </View>
-      <View style={{ flexDirection: 'row' }}>
-        {/* <View style={styles.button}>
-          <Button
-            title='로그인'
-            onPress={() => navigation.navigate("Login")}
-          />
-        </View>
-        <View style={styles.button}>
-          <Button
-            title='회원가입'
-            onPress={() => navigation.navigate("Signup")}
-          />
-        </View> */}
+      <TouchableOpacity>
+        <KakaoText>카카오로 빠르게 시작하기</KakaoText>
+      </TouchableOpacity>
+      <ButtonView>
         <TouchableOpacity
-        	activeOpacity={0.5} //깜빡임을 조절하는 기능
->
-          <Asd style={{backgroundColor:"red"}}>로그인</Asd>
+          activeOpacity={0.8}
+          onPress={() => navigation.navigate("Login")}>
+          <MoveText>로그인</MoveText>
         </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => navigation.navigate("Signup")}>
+          <MoveText>회원가입</MoveText>
+        </TouchableOpacity>
+      </ButtonView>
+    </Sort>
   )
 }
 
-const styles = StyleSheet.create({
-  sort: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#FFE3ED'
-  },
-  kakaoButton: {
-    backgroundColor: 'yellow',
-    width: 240,
-    borderRadius: 15,
-    marginBottom: 10,
-    borderWidth: 1,
-  },
-  button: {
-    backgroundColor: '#BEEBE9',
-    width: 140,
-    borderRadius: 15,
-    marginBottom: 10,
-    borderWidth: 1,
-  }
-});
-
-const Nbt = styled.View`
-  background-color: '#red';
-  width: 120px;
-  border-radius: 12;
-  margin-bottom: 10px;
-  border: 1px solid red;
+const Sort = styled.SafeAreaView`
+  flex: 1;
+  align-items: center;
+  background-color: white;
 `
 
 const Logo = styled.View`
@@ -73,9 +39,28 @@ const Logo = styled.View`
   margin: 40px 0 60px 0;
 `
 
-const Asd =styled.Text`
-  width: 200px;
-  height: 200px;
+const ButtonView = styled.View`
+  flex-direction: row;
+`
+
+const KakaoText = styled.Text`
+  padding: 12px 84px;
+  background-color: #FEE500;
+  border-radius: 12px;
+  margin-bottom: 10px;
+  overflow: hidden;
+
+  font-weight: 600;
+`
+
+const MoveText = styled.Text`
+  padding: 12px 24px;
+  background-color: #BEEBE9;
+  border: 1px solid #9BE3DE;
+  border-radius: 12px;
+  overflow: hidden;
+  color: #FFFDF9;
+  font-weight: 800;
 `
 
 
