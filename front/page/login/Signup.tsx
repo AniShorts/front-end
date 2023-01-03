4dda76f48b5d82f4fc5b44a8d909681a8a37bdd0
윤선용 <sunyong9059@naver.com>
2023-01-03 15:15:31 +0900

    회원가입 view 초안

--- front/page/login/Signup.tsx
+++ front/page/login/Signup.tsx
@@ -1,80 +1,142 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button} from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { TextInput } from 'react-native-gesture-handler'
+import { Text, TouchableOpacity } from 'react-native'
 import styled from 'styled-components/native'
+import { useState } from 'react'
 
 const Signup = ({ navigation }: any) => {
 
+  const [checked, setChecked] = useState(false)
+
   return (
-    <SafeAreaView style={styles.sort}>
-      <Logo />
-      <View style={{flexDirection:'row'}}>
-        <TextInput
-          style={styles.customInput}
+    <Sort>
+      <Logo
+        source={require('../shared/TestLogo.jpg')}
+      />
+
+      <Text>ID</Text>
+      <AlignView>
+        <CustomInput
           placeholder='닉네임을 입력해주세요'
         />
-       <Text style={styles.asd}>asd</Text>
-      </View>
-      <TextInput
-          style={styles.customInput}
-          placeholder='비밀번호를 입력해주세요'
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => navigation.navigate("Signup")}>
+          <Check>중복확인</Check>
+        </TouchableOpacity>
+      </AlignView>
+
+      <Text>비밀번호</Text>
+      <CustomInputPassword
+        placeholder='비밀번호를 입력해주세요'
+      />
+
+      <Text>비밀번호 확인</Text>
+      <CustomInputPassword
+        placeholder='비밀번호를 한번 더 입력하세요'
+      />
+      <Text>전화번호</Text>
+      <AlignView>
+        <CustomInput
+          placeholder='전화번호를 입력해주세요'
         />
-        <TextInput
-          style={styles.customInput}
-          placeholder='비밀번호를 한번 더 입력하세요'
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => navigation.navigate("Signup")}>
+          <Check>인증하기</Check>
+        </TouchableOpacity>
+      </AlignView>
+
+      <AlignView>
+        <CustomInput
+          placeholder='인증번호를 입력해주세요'
         />
-        <View style={{flexDirection:'row'}}>
-        <TextInput
-          style={styles.customInput}
-          placeholder='닉네임을 입력해주세요'
-        />
-       <Text style={styles.asd}>asd</Text>
-      </View>
-      <View style={{flexDirection:'row'}}>
-        <TextInput
-          style={styles.customInput}
-          placeholder='닉네임을 입력해주세요'
-        />
-       <Text style={styles.asd}>asd</Text>
-      </View>
-    </SafeAreaView>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => navigation.navigate("Signup")}>
+          <Check>인증확인</Check>
+        </TouchableOpacity>
+      </AlignView>
+
+      <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => navigation.navigate("Signup")}>
+            {
+              checked ? <SignupText>회원가입</SignupText>  : <SignupText2>회원가입</SignupText2>
+            }
+        </TouchableOpacity>
+    </Sort>
   );
 }
 
-const styles = StyleSheet.create({
-  sort: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#FFE3ED'
-  },
-  customInput: {
-    padding: 10,
-    fontSize: 18,
-    backgroundColor: "#eee",
-    width: 230,
-    borderRadius: 8,
-    borderWidth: 1,
-    marginTop:30
-  },
-  button: {
-    flexDirection: "row",
-    backgroundColor: '#9BE3DE',
-    width: 10,
-    borderRadius: 4,
-  },
-  asd: {
-    backgroundColor:'red'
+const Sort = styled.SafeAreaView`
+  flex: 1;
+  align-items: center;
+  background-color: white;
+`
 
-  }
-});
-
-const Logo = styled.View`
+const Logo = styled.Image`
   width: 70%;
   height: 100px;
   margin-top: 10%;
-  margin-bottom: 30px;
-  border: 1px solid red;
+  margin-bottom: 10%;
+  border: 1px solid black;
+`
+
+const AlignView = styled.View`
+  flex-direction: row;
+`
+
+const CustomInput = styled.TextInput`
+  width: 60%;
+  height: 40px;
+  padding: 8px 10px;
+  margin-bottom: 24px;
+  margin-right: 10px;
+
+  border-radius: 6px;
+  border: 1px solid rgb(209, 213, 219);
+`
+const CustomInputPassword = styled.TextInput`
+  width: 80%;
+  height: 40px;
+  padding: 8px 10px;
+  margin-bottom: 24px;
+  margin-right: 10px;
+
+  border-radius: 6px;
+  border: 1px solid rgb(209, 213, 219);
+`
+
+const Check = styled.Text`
+  padding: 12px;
+  background-color: #BEEBE9;
+  border: 1px solid #9BE3DE;
+  border-radius: 12px;
+
+  overflow: hidden;
+  color: #FFFDF9;
+  font-weight: 800;
+`
+
+const SignupText = styled.Text`
+  padding: 12px 100px;
+  background-color: #BEEBE9;
+  border: 1px solid #9BE3DE;
+  border-radius: 12px;
+  overflow: hidden;
+  color: #FFFDF9;
+  font-weight: 800;
+`
+
+const SignupText2 = styled.Text`
+  padding: 12px 100px;
+  background-color: #beebe96a;
+  opacity: 0.5;
+  border: 1px solid #BEEBE9;
+  border-radius: 12px;
+  overflow: hidden;
+  color: #FFFDF9;
+  font-weight: 800;
 `
 
 export default Signup
