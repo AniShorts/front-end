42b49cd56fd3f58a29f199bf6399e0df1c8160d4
윤선용 <sunyong9059@naver.com>
2023-01-10 18:06:37 +0900

    Introduce 페이지 동영상 임시처리

--- front/page/login/Introduce.tsx
+++ front/page/login/Introduce.tsx
@@ -1,29 +1,31 @@
-import React, { useState, useRef} from "react";
-import {TouchableOpacity, Text } from 'react-native'
+import React, { useState, useRef } from "react";
+import { View, StyleSheet, Button, TouchableOpacity, Text } from 'react-native';
+import { Video, AVPlaybackStatus } from 'expo-av';
 import styled from 'styled-components/native'
-import { Video } from "expo-av";
 import { Feather } from '@expo/vector-icons';
 
-
 const Introduce = ({ navigation }: any) => {
-
   const video = useRef(null);
   const [status, setStatus] = useState({});
 
   return (
     <Sort>
-      <Logo>
-        <Video 
-           ref={video}
-           source={require('../shared/VideoTest.mp4')}
-           useNativeControls
-           isLooping
-           onPlaybackStatusUpdate={status => setStatus(() => status)}
+      <View>
+        <Video
+          ref={video}
+          style={styles.video}
+          source={{
+            uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+          }}
+          useNativeControls
+          resizeMode="contain"
+          isLooping
+          onPlaybackStatusUpdate={status => setStatus(() => status)}
         />
-      </Logo>
-      <TouchableOpacity style={{position:"relative"}}>
+      </View>
+      <TouchableOpacity style={{ position: "relative" }}>
         <KakaoText>카카오로 빠르게 시작하기</KakaoText>
-        <Feather name="message-circle" size={24} color="black" style={{position:"absolute", top:9, left:12}}/>
+        <Feather name="message-circle" size={24} color="black" style={{ position: "absolute", top: 9, left: 12 }} />
       </TouchableOpacity>
       <ButtonView>
         <TouchableOpacity
@@ -38,20 +40,34 @@ const Introduce = ({ navigation }: any) => {
         </TouchableOpacity>
       </ButtonView>
     </Sort>
-  )
+  );
 }
+
+export default Introduce
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  video: {
+    alignSelf: 'center',
+    width: 300,
+    height: 550,
+    marginBottom: 30
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 const Sort = styled.SafeAreaView`
   flex: 1;
   align-items: center;
   background-color: white;
-`
-
-const Logo = styled.View`
-  width: 280px;
-  height: 500px;
-  border: 1px solid red;
-  margin: 40px 0 60px 0;
 `
 
 const ButtonView = styled.View`
@@ -62,7 +78,7 @@ const KakaoText = styled.Text`
   padding: 12px 84px;
   background-color: #FEE500;
   border-radius: 12px;
-  margin-bottom: 10px;
+  margin-bottom: 20px;
   overflow: hidden;
 
   font-weight: 600;
@@ -78,6 +94,3 @@ const MoveText = styled.Text`
   font-weight: 800;
   margin : 0px 10px 0px 10px;
 `
-
-
-export default Introduce
